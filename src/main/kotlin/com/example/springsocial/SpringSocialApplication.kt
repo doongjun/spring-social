fcdad23df8b5ce828d0bb061ec2d818132a0235b
doongjun <doongjun@kakao.com>
2023-05-18 18:00:56 +0900

    init project

---
+++ src/main/kotlin/com/example/springsocial/SpringSocialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springsocial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSocialApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSocialApplication>(*args)
+}
